29e0cca29da39c4148b179aed89902ceceba45b4
netonblpb <skullbog@gmail.com>
2022-02-11 15:56:32 +0300

    Task31 complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+Console.Clear();
+
+int[] mass = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = rnd.Next(10, 100);
+    Console.Write(mass[i] + "     ");
+}
